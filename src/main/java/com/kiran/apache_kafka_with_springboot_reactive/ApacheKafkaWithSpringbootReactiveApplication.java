41d5c2c343d831b33eb25315ae93097deab32de2
Kirankumar866 <Kirankumarparasa9996@gmail.com>
2024-11-29 13:24:06 -0600

    Setup done

---
+++ src/main/java/com/kiran/apache_kafka_with_springboot_reactive/ApacheKafkaWithSpringbootReactiveApplication.java
@@ -0,0 +1,13 @@
+package com.kiran.apache_kafka_with_springboot_reactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApacheKafkaWithSpringbootReactiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApacheKafkaWithSpringbootReactiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kiran/apache_kafka_with_springboot_reactive/ApacheKafkaWithSpringbootReactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kiran.apache_kafka_with_springboot_reactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApacheKafkaWithSpringbootReactiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
