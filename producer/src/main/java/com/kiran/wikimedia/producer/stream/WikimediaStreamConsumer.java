b3549e5b8d12f2902ad9c4062d53fe8b8105c2a0
Kirankumar866 <Kirankumarparasa9996@gmail.com>
2024-12-01 11:20:15 -0600

    upon running with wikimedia stream

--- producer/src/main/java/com/kiran/wikimedia/producer/stream/WikimediaStreamConsumer.java
+++ producer/src/main/java/com/kiran/wikimedia/producer/stream/WikimediaStreamConsumer.java
@@ -23,7 +23,7 @@ public class WikimediaStreamConsumer {
                 .uri("/stream/recentchange")
                 .retrieve()
                 .bodyToFlux(String.class)
-                .subscribe(log::info);
+                .subscribe(producer:: sendMessage);
 
     }
 }
